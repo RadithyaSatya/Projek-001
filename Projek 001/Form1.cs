2f8b737d3fc5dee5eca3882f85e77b1424c2d47d
Tio <Tio@DESKTOP-S82G5I6>
2022-11-19 12:35:55 +0700

    login design

--- Projek 001/Form1.cs
+++ Projek 001/Form1.cs
@@ -6,5 +6,31 @@ namespace Projek_001
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(textBox2.PasswordChar=='*')
+                textBox2.PasswordChar= '\0';
+            else
+                textBox2.PasswordChar= '*';
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult a1 = MessageBox.Show("KAMU YAKIN INGIN KELUAR?", "Warning", MessageBoxButtons.YesNo);
+            if (a1 == DialogResult.Yes)
+            Application.Exit();
+         
+        }
     }
 }
